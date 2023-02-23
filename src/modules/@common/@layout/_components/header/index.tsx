064f74ba3df2fbeaf2eceec068a8e7724042c564
mizanm4yourslab <mizan@m4yours.com>
2023-02-23 10:26:53 +0600

    layout

---
+++ src/modules/@common/@layout/_components/header/index.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <header className="border-b bg-[#f5f6fa;]">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div className="left">left</div>
+          <div className="right">right</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/modules/website/index.tsx
@@ -0,0 +1,11 @@
+export const Website = () => {
+  return (
+    <div>
+      website
+      <div>website this is tes</div>
+      <div>lorem ipsum dolor sit amet, consectetur adip</div>
+    </div>
+  );
+};
+
+export default Website;
